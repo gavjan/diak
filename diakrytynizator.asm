98e9bb1858a0808e3c20eec857383415d4d3381a
gavjan <gavjan@github.com>
2021-03-30 18:06:52 +0200

    ready to submit

--- diakrytynizator.asm
+++ diakrytynizator.asm
@@ -1,10 +1,10 @@
-%include "macros.inc"
+;%include "macros.inc"
 
-;SYS_READ    equ 0
-;SYS_WRITE   equ 1
-;SYS_EXIT    equ 60
-;STDIN   	equ 0
-;STDOUT  	equ 1
+SYS_READ    equ 0
+SYS_WRITE   equ 1
+SYS_EXIT    equ 60
+STDIN   	equ 0
+STDOUT  	equ 1
 
 section .bss
 	argc resb 8
